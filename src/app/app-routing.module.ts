b86bfeda1b353353586e8fedae15e2e7e3b04a4a
AndresRamirez9912 <andres.ramirez9912@gmail.com>
2022-02-23 13:22:02 -0500

    Finalizacion-FrontEnd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { ItemsComponent } from './components/items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', // aca va la direccion del index
+    component: ItemsComponent // Componente que quiero cargar en dicha direccion (en este caso el index)
+  },
+  {
+    path:'New_Item',
+    component:AddItemComponent // Quiero que cargue este componente en la direccion add
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
